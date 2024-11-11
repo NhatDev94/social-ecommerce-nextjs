6c6c9e51a31f33b87a66dac48a8901ca4f6e295a
nhatdev94 <nhatdev94@gmail.com>
2024-11-11 17:10:07 +0700

    chore: add comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,10 @@ export default function Home() {
   return (
     <div className="w-full h-[calc(100dvh-64px)] bg-gray-200 p-4 overflow-y-scroll">
       <div className="hero w-full h-fit mb-4 bg-yellow-200">
+        {/* 
+          1. Delete class bg
+          2. add skeleton for hero
+        */}
         <Image
           src="https://images.pexels.com/photos/28271474/pexels-photo-28271474/free-photo-of-thanh-ph-k-ngh-ngh-thu-t-toa-nha.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
           alt="image"
